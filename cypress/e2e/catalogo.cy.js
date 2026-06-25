783688490feed4b3498042432b505647843523e9
Sarah Sant'Ana <sarah.santana1207@gmail.com>
2026-06-25 16:47:11 -0300

    Automaçao do Contato

---
+++ cypress/e2e/catalogo.cy.js
@@ -0,0 +1,42 @@
+/// <reference types= "cypress"/>
+
+describe('Funcionalide: Catalogo de livros', () => {
+
+    beforeEach(() => {
+        cy.visit('catalog.html')
+    });
+
+it.skip('Deve clicar no botão Adicionar à cesta', () => {
+    cy.get(':nth-child(1) > .card > .card-body > .mt-auto > .d-grid > .btn-primary').click()
+    cy.get('#cart-count').should('contain', 1)
+});
+
+it('Deve clicar em todos os botões Adicionar à cesta', () => {
+   cy.get('.btn-primary').click({ multiple: true }) 
+});
+
+it('Deve clicar no primeiro botão Adicionar à cesta', () => {
+    cy.get('.btn-primary').first().click()
+});
+
+it('Deve clicar no último botão Adicionar à cesta', () => {
+    cy.get('.btn-primary').last().click()
+});
+
+it('Deve clicar no terceiro botão Adicionar à cesta', () => {
+    cy.get('.btn-primary').eq(2).click()
+});
+
+it('Deve clicar no quinto botão Adicionar à cesta', () => {
+    cy.get('.btn-primary').eq(4).click()
+    cy.get('#global-alert-container').should('contain', 'A Metamorfose')
+});
+
+it('Deve cliclar no nome do livro e direcionar para tela do livro', () => {
+    cy.contains('Dom Casmurro').click()
+    cy.url().should('include', 'book-details')
+    cy.get('#add-to-cart-btn').click()
+    cy.get('#alert-container').should('contain', 'Livro adicionado à cesta com sucesso!')
+});
+
+});
